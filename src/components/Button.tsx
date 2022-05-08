fff73c323fbda4823ac8c07b4446a3ea64c11a5a
Vitor Guerra <vitorguerra.dev@gmail.com>
2022-05-08 00:18:23 -0300

    Correção: importação do ThemeContext no Button.tsx

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { TouchableOpacity, Text } from "react-native";
-import { ThemeContext } from "../context/themeContext";
+import { ThemeContext } from "../context/ThemeContext";
 import { Styles } from "../styles/GlobalStyles";
 
 interface ButtonProps {
